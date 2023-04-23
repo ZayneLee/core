776577d396e71082b8cd8eff3aa13ca10ad565d2
Zayne <star1582@naver.com>
2023-04-23 21:42:18 +0900

    다양한 의존관계 주입 방법법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,11 +10,20 @@ import hello.core.member.MemberRepository;
 @Component // 구현체 클래스에 사용
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    // @Autowired 필드 주입, 권장하지 않음, DI 프레임워크가 없으면 사용 불가
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
-    @Autowired // 생성자에 사용
+    @Autowired // 생성자에 사용 , 생성자가 딱 1개만 있으면 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("====================== 1. OrderServiceImpl");
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("====================== 2. init");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
